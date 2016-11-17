57573550264fe1368d9df018ea7b5b489bdce210
mateuscfonseca <mateuscfonseca@gmail.com>
2016-11-17 17:12:53 -0200

    Removendo arquivos desnecessários

--- Sistemas e Politicas de Educacao/Avaliação - Os nove jeitos mais comuns de avaliar os estudantes e os benefícios de cada um_files/bola.js
+++
@@ -1,9 +0,0 @@
-function removeIframeByName(name) {
-	iframeCollection = document.getElementsByName(name);
-
-	for (var i=0; iframe = iframeCollection[i++];) {
-		iframe.width = 0;
-		iframe.height = 0;
-		iframe.src = "";
-	}
-}
